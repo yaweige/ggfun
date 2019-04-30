a1b12b70dba62dc954bcbb4c3ffec020d0f080c4
WeiquanLuo <weiquan.luo1221@gmail.com>
2019-04-30 01:28:28 -0500

    update test-geom_iamge.r according to the gom_image.r improvement

--- tests/testthat/test-geom_image.R
+++ tests/testthat/test-geom_image.R
@@ -1,9 +1,11 @@
 context("test-geom_image")
 
 test_that("output is a ggplot2 plot", {
+  p <- system.file("extdata", "images.jpg", package = "ggfun")
+  img <- magick::image_read(p)
   p <- ggplot(data = data.frame(x = rnorm(10, mean = 0, sd = 1),
                                 y = rnorm(10, mean = 0, sd = 1))) +
-    geom_image(aes(x = x, y = y), size = 0.5, na.rm = TRUE)
+    geom_image(aes(x = x, y = y), size = 0.05, img =img, na.rm = TRUE)
   expect_is(p, "ggplot")
 })
 
